dbafe15bc4250fa9be6919827413aaa95848fc1e
Christian Geroy <cr50.christian@enfrausaph.com>
2018-08-17 17:35:13 -0400

    Adding files created for heroku and initial commit on frontend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 //Routers
 const authors = require('./routes/api/author');
@@ -23,6 +24,17 @@ app.use(bodyParser.urlencoded({extended:false}));
 // Use Routes
 app.use('/authors', authors);
 app.use('/posts', posts);
+
+// Serve Static assets if in production
+
+if(process.env.NODE_ENV === 'production'){
+  //Set Static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //Connect to the server
 const port = process.env.PORT || 5000;
