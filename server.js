2a2faf3b3b84cd1494b84eb363888b473f144a84
Christian Geroy <geroychristian@gmail.com>
2018-08-21 09:37:58 +0800

    Adding CORS

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+//CORS
+
+app.use((req,res,next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-type');
+  next();
+})
+
 // Use Routes
 app.use('/authors', authors);
 app.use('/posts', posts);
