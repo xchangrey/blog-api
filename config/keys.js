362d51652bacb46f0cbe7cf73668f763d0dcb396
xchangrey <geroychristian@gmail.com>
2022-10-22 16:40:35 +0800

    fix secrets

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://geroychristian:ixFJEU64xugLQE4p@cluster0.hs7ehy9.mongodb.net/?retryWrites=true&w=majority"
+  mongoURI: "your_application_connection_here"
 };
